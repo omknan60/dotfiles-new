bdde0675bb22cce72f583b399d02951fb290134d
Omkar Nandan <omknan60@gmail.com>
2022-10-17 18:24:06 +0530

    edit to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ sudo pacman -Syy
 sudo pacman -S yay neovim foot qutebrowser foot-terminfo wayland wlroots sway ttf-nerd-fonts-symbols-common ttf-sourcecodepro-nerd --needed
 
 yay -S tofi i3status-rust-git --needed
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+cd ~/dotfiles-new
+
+stow nvim foot i3status sway qutebrowser 
