f654b65de66ab47cb5ea28280eeb13f0f77f850d
Omkar Nandan <omknan60@gmail.com>
2022-11-18 10:32:09 +0530

    Changes to almost everything

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -108,8 +108,8 @@ cmp.setup({
 		end,
 	},
 	sources = {
-		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
+		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
