fb265cb6f388d079dcfb6857c12cf504e8d4678f
Omkar Nandan <omknan60@gmail.com>
2022-12-03 16:31:35 +0530

    cleaned up directory

--- nvim-archive/.config/nvim/lua/user/colorscheme.lua
+++
@@ -1,7 +0,0 @@
---vim.cmd('colorscheme tokyonight-night')
---vim.cmd('colorscheme tokyonight-storm')
---vim.cmd('colorscheme tokyonight-moon')
-vim.cmd('colorscheme gruvbox')
---vim.cmd('colorscheme rose-pine')
---vim.cmd('colorscheme catppuccin-mocha')
-vim.opt.background="dark"
